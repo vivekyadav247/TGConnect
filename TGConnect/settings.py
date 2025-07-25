09f179b3cd483b4b2db24bd17d8dcc2dbac1f065
Vivek Yadav <vivekyad240706@gmail.com>
2025-07-25 19:21:30 +0530

    solve

--- TGConnect/settings.py
+++ TGConnect/settings.py
@@ -110,7 +110,8 @@ USE_TZ = True
 # settings.py
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
