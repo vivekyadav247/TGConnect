7e1cbbc0c4aa9ebf2d45c68ae4376ed85d77125d
Vivek Yadav <vivekyad240706@gmail.com>
2025-07-26 10:45:01 +0530

    Postgre SQL IN Local

--- TGConnect/settings.py
+++ TGConnect/settings.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,9 +106,12 @@ WSGI_APPLICATION = 'TGConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# settings.py
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
