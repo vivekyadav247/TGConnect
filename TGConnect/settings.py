73d9bd90deaef190df7ae122155f9f9f56964a52
Vivek Yadav <vivekyad240706@gmail.com>
2025-07-26 09:49:45 +0530

    Fresh migration with Django default table names

--- TGConnect/settings.py
+++ TGConnect/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'TGConnect.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Password validation
