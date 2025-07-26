a1161ec5b5015ab364230766895058f1e75e9b70
Vivek Yadav <vivekyad240706@gmail.com>
2025-07-26 09:03:33 +0530

    Convert to PostgreSQL for production deployment - All issues fixed

--- TGConnect/settings.py
+++ TGConnect/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,10 +104,11 @@ WSGI_APPLICATION = 'TGConnect.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True if config('DATABASE_URL', default='') else False
+    )
 }
 
 
